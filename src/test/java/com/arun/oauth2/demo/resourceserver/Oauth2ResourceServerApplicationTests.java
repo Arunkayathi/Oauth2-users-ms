4defcd9aa03616e76e3b68aa39c2cc00fd90ca3c
Arun <arunreddy.kayathi@gmail.com>
2021-03-27 17:01:30 -0500

    Initial Commit

---
+++ src/test/java/com/arun/oauth2/demo/resourceserver/Oauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.oauth2.demo.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
