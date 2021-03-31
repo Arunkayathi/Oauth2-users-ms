56f31a0d4f08de3788474a21d4695f21a85efac6
Arun <arunreddy.kayathi@gmail.com>
2021-03-30 22:37:44 -0500

    Enable method level security and @secured annotation

--- src/main/java/com/arun/oauth2/demo/resourceserver/security/WebSecurity.java
+++ src/main/java/com/arun/oauth2/demo/resourceserver/security/WebSecurity.java
@@ -1,15 +1,15 @@
 package com.arun.oauth2.demo.resourceserver.security;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
