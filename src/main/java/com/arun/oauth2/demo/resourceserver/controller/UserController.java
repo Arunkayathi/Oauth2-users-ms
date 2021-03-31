0b18a023dd09b4a8d7d6b17250792830a45d593f
Arun <arunreddy.kayathi@gmail.com>
2021-03-30 23:06:21 -0500

    Add @PostAuthorize annotation example

--- src/main/java/com/arun/oauth2/demo/resourceserver/controller/UserController.java
+++ src/main/java/com/arun/oauth2/demo/resourceserver/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.arun.oauth2.demo.resourceserver.controller;
 
+import com.arun.oauth2.demo.resourceserver.dto.UserDetails;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -21,4 +23,11 @@ public class UserController {
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
         return "Deleted user with id "+id+" and jwt user id : "+jwt.getSubject();
     }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping("/{id}")
+    public UserDetails getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return new UserDetails("Arun", "Reddy", id);
+    }
 }
+
---
+++ src/main/java/com/arun/oauth2/demo/resourceserver/dto/UserDetails.java
@@ -0,0 +1,57 @@
+package com.arun.oauth2.demo.resourceserver.dto;
+
+import java.util.Objects;
+
+public class UserDetails {
+
+    private String firstName;
+    private String lastName;
+    private String userId;
+
+    public UserDetails(String firstName, String lastName, String userId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDetails that = (UserDetails) o;
+        return Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, userId);
+    }
+
+}
